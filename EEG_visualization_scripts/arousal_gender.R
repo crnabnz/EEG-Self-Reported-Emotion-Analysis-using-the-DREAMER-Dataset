d88e5fde862b5c4e263553f7f7e6028cafdd757f
crnabnz <cerenabnz@gmail.com>
2025-07-20 21:55:51 +0000

    Delete EEG_visualization_scripts/arousal_gender.R

--- EEG_visualization_scripts/arousal_gender.R
+++
@@ -1,9 +0,0 @@
-gender_colors <- c("female" = "#FBB4B9", "male" = "#B3CDE3")
-
-ggplot(data, aes(x = gender, y = arousal, fill = gender)) +
-  geom_violin(trim = FALSE, alpha = 0.6) +
-  geom_boxplot(width = 0.1, outlier.shape = NA) +
-  scale_fill_manual(values = gender_colors) +
-  labs(title = "Arousal Ratings by Gender", x = "Gender", y = "Arousal") +
-  theme_minimal() +
-  theme(legend.position = "none")
